ac4eb39d2770b14a8e7fbb39494ac6e357232967
Aedan <anmagsom@uci.edu>
2026-01-14 12:00:13 -0800

    Mobile reformatting and random gallery
    Added CSS mobile format specifiers following bootstrap's LG device width guidelines. Added random image algorithm for displaying gallery images.

--- js/photography.js
+++ js/photography.js
@@ -3,14 +3,14 @@
 const modal = new bootstrap.Modal(document.getElementById("imageViewer"));
 // Gets the div hosting the modalviewer
 const modalImg = document.getElementById("modalImage");
+// Get div hosting the images itself
+const imagesGallery = document.getElementById("images");
+
 
 function onClick(img){
     modalImg.src = img.src;
     modal.show();
 }
-
-// Get div hosting the images itself
-const imagesGallery = document.getElementById("images");
 
 function createImages(){
 
@@ -18,6 +18,12 @@ function createImages(){
     const filepath = "./../img/photos/img";
     const file_extension = ".jpg";
     const num_images = 92;
+    const imageNumbers = [];
+
+    // Create array for sampling
+    for (let i = 0; i < num_images; i++){
+        imageNumbers[i] = i;
+    }
 
     for (let i = 0; i < num_images; i++){
 
@@ -25,11 +31,17 @@ function createImages(){
         let img = document.createElement("img");
         img.addEventListener("click", function() { onClick(img)});
 
-        // Just for image formatting
-        img.src = filepath + i + file_extension;
-    
-        console.log(img.src);
+        // Choose random image to add. Selects an index randomly
+        const randomImage = Math.floor(Math.random() * imageNumbers.length);
+        // console.log(imageNumbers, randomImage, imageNumbers[randomImage]);
 
+        // Change image source
+        img.src = filepath + imageNumbers[randomImage] + file_extension;
+
+        // Remove the value from the array
+        imageNumbers.splice(randomImage, 1);
+    
+        console.log(img.src + " created and added to HTML.");
         img.className = "img-fluid gallery-img";
 
         // Add to div
